ee1d2c17032c585b18311c57ac72b1d22c220b1d
katty-azabache <kattyazabachez@gmail.com>
2015-07-01 15:50:04 -0500

    nav casi terminado

--- mi-script.js
+++ mi-script.js
@@ -1,31 +1,32 @@
 // deslis lento
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-  $('html,body').animate({
-     scrollTop: target.offset().top - 0 
-    } , 1000);
-      return false;
-    }
-  }
- });
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 0 
+				} , 1000);
+				return;
+			}
+		}
+	});
 });
 
 // slider
 $(function(){
-    $('#slider div:gt(0)').hide();
-    setInterval(function(){
-      $('#slider div:first-child').fadeOut(0)
-         .next('div').fadeIn(900)
-         .end().appendTo('#slider');}, 4000);
+	$('#slider div:gt(0)').hide();
+	setInterval(function(){
+		$('#slider div:first-child').fadeOut(0)
+		.next('div').fadeIn(900)
+		.end().appendTo('#slider');}, 4000);
 });
 
 // menu
 $(".menu-collapsed").click(function() {
-  $(this).toggleClass("menu-expanded");
+	$(this).toggleClass("boton-menu");
 });
+
 
 
